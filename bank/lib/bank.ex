c4cf71d1a5a57b03ac6161c34c731c697e66b6f7
rivercodi <rivercodi@gmail.com>
2017-06-13 21:06:47 -0700

    bank

--- bank/lib/bank.ex
+++ bank/lib/bank.ex
@@ -1,18 +1,20 @@
 defmodule Bank do
-  @moduledoc """
-  Documentation for Bank.
-  """
+  defstruct balance: 0
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bank.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def new(balance) do 
+    %__MODULE__{balance: balance}
   end
+
+  def deposit(account, amount) do 
+    %{account | balance: account.balance + amount}
+  end
+
+  def withdraw(account, amount) do 
+    %{account | balance: account.balance - amount}
+  end
+
+  def balance(account) do
+    account.balance
+  end
+
 end
