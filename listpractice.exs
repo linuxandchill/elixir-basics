4914a0a13b40cdb0ab53e885cbcaf20101be484e
linuxandchill <rivercodi@gmail.com>
2017-08-06 14:49:27 -0700

    map comprehension

--- listpractice.exs
+++ listpractice.exs
@@ -4,7 +4,9 @@ defmodule Listpractice do
   def square([]) do
     []
   end
-  #if list not empty
+  #if list not empty return 
+  #new_head = old_head x itself
+  #new_tail = list of squares of the tail
   def square([head | tail]) do
     [head * head | square(tail)]
   end
