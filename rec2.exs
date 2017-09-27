6a14115ec01a570fb8ca61e447325afc7c6e43a2
rivercodi <rivercodi@gmail.com>
2017-09-26 21:56:44 -0700

    tail-call optimized recursive funcs

--- rec2.exs
+++ rec2.exs
@@ -1,6 +1,17 @@
 defmodule Fact do 
-  def fac(num) when num <= 1, do: 1
+
   #not tail-call optimized
-  def fac(num), do: num * fac(num - 1) 
+  #def fac(num) when num <= 1, do: 1
+  #def fac(num), do: num * fac(num - 1) 
+  
+  def fac(num, acc \\ 1)
+
+  def fac(0, acc) do 
+    acc
+  end
+
+  def fac(num, acc) do 
+    fac(num-1, acc * num )
+  end
 
 end
